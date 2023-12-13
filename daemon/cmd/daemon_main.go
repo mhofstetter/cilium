d03ed0147eeaa21e457f164aa7054057dc9141d7
Marco Hofstetter <marco.hofstetter@isovalent.com>
2023-12-13 15:13:58 +0100

    k8s watcher: replace envoyConfigManager with existing interfaces
    This commit replaces the interface `envoyConfigManager` with the
    two existing interfaces `envoy.XdsServer` & `envoy.PortAllocator`.
    
    This is a first step in making the `Proxy` independent from the
    Envoy `XdsServer`.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -1641,6 +1641,7 @@ type daemonParams struct {
 	L2Announcer          *l2announcer.L2Announcer
 	ServiceManager       service.ServiceManager
 	L7Proxy              *proxy.Proxy
+	EnvoyXdsServer       envoy.XDSServer
 	DB                   *statedb.DB
 	APILimiterSet        *rate.APILimiterSet
 	AuthManager          *auth.AuthManager
